233e53ef8841b67f2283d3e212a97c1623bb09f0
lokie2003 <141050042+lokie2003@users.noreply.github.com>
2023-10-30 23:28:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,55 @@
+# app.py
+import os
 from flask import Flask, request, render_template, Response
+from werkzeug.utils import secure_filename
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
 import numpy as np
-import tensorflow as tf
-import pathlib
-import tensorflow_hub as hub
-from PIL import Image
 import cv2
 
-app = Flask(__name)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
-# Register the custom KerasLayer
-hub_layer = hub.KerasLayer("https://tfhub.dev/google/imagenet/resnet_v2_50/feature_vector/4", trainable=False)
+# Load the pre-trained MobileNetV2 model
+model = MobileNetV2(weights='imagenet')
 
-# Define the emotion labels
-emotion_labels = ['angry', 'happy', 'relaxed', 'sad']
-
-# Load the pre-trained model
-def load_model():
-    with tf.keras.utils.custom_object_scope({'KerasLayer': hub_layer}):
-        model = tf.keras.models.load_model('dog_emotion.h5')
-    return model
-
-model = load_model()
-
-# Define a function to preprocess the image using Pillow (PIL)
-def preprocess_image_pil(image):
-    img = image.resize((224, 224))
-    img = np.array(img) / 255.0
+def preprocess_image(img):
+    img = cv2.resize(img, (224, 224))
+    img = image.img_to_array(img)
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
     return img
 
-def gen_frames():
-    video_capture = cv2.VideoCapture(0)
+def predict_emotion(image):
+    preds = model.predict(image)
+    decoded_preds = decode_predictions(preds)
+    # Process the decoded predictions and return the result (emotion prediction)
+
+def camera():
+    cap = cv2.VideoCapture(0)
     while True:
-        success, frame = video_capture.read()
-        if not success:
+        ret, frame = cap.read()
+        if not ret:
             break
-        # Resize frame to 224x224 (assuming ResNet input size)
-        frame = cv2.resize(frame, (224, 224))
-        # Convert the frame to RGB
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Make a prediction
-        processed_image = frame_rgb / 255.0
-        prediction = model.predict(np.expand_dims(processed_image, axis=0))
-        predicted_class_index = np.argmax(prediction)
-        predicted_class = emotion_labels[predicted_class_index]
-        predicted_class_probability = prediction[0][predicted_class_index]
-        # Display the prediction on the frame
-        cv2.putText(frame, f'Predicted Emotion: {predicted_class}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.putText(frame, f'Probability: {predicted_class_probability:.2f}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        # Encode the frame as JPEG
+
+        img = preprocess_image(frame)
+        prediction = predict_emotion(img)
+
+        cv2.putText(frame, f"Prediction: {prediction}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        if ret:
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-    return render_template('camera.html')
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
